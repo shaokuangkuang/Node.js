7e9b9fc75f7d566da06f29364e0340aa11f58633
shaokuang <314462224@qq.com>
2019-09-19 19:53:07 +0800

    add 9.19

---
+++ 03-console/01-format.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+var user = { name:'邵贶',
+            qq: '21312432141'
+};
+console.log('name：%s',user.name);
+console.log("age:%d",40);
+console.log("JSON: %j",user);
+
+console.error("Eoor! something wrong");
